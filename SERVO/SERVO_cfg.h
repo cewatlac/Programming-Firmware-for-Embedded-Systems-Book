61c3e4b9141d6748c7bb7519c4a9122a16bac3c5
Tanjiro4700 <hassanomar4700@gmail.com>
2024-12-01 20:35:08 +0200

    Create SERVO_cfg.h

---
+++ SERVO/SERVO_cfg.h
@@ -0,0 +1,10 @@
+#ifndef SERVO_CFG_H_
+#define SERVO_CFG_H_
+
+// Include the header for the SERVO module where the struct and function prototypes are defined
+#include "SERVO.h"
+
+// External declaration of the servo configuration array, which holds configurations for all the servo motors
+extern const SERVO_CfgType SERVO_CfgParam[SERVO_NUM];
+
+#endif /* SERVO_CFG_H_ */
