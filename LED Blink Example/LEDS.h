3aa08ef86630ec5b57dee8a997e2322e0e318e7f
Tanjiro4700 <hassanomar4700@gmail.com>
2024-12-02 15:00:55 +0200

    Create LEDS.h

---
+++ LED Blink Example/LEDS.h
@@ -0,0 +1,37 @@
+
+#ifndef LEDS_H_
+#define LEDS_H_
+
+#define HAL_GPIO_MODULE_ENABLED
+
+#include "stm32f1xx_hal.h"
+
+
+// The Number OF LED Units To Be Used In The Project
+#define LED_UNITS  2
+
+
+typedef struct
+{
+	GPIO_TypeDef * LED_GPIO;
+	/*
+	 * GPIOA
+	 * GPIOB
+	 * ...
+	 * */
+	uint16_t       LED_PIN;
+	/*
+	 * GPIO_PIN_0
+	 * GPIO_PIN_1
+	 * ...
+	 * */
+}LED_CfgType;
+
+
+/*-----[ Prototypes For All Functions ]-----*/
+
+void LEDs_Init(void);
+void LED_ON(uint16_t au16_Instance);
+void LED_OFF(uint16_t au16_Instance);
+
+#endif /* LEDS_H_ */
