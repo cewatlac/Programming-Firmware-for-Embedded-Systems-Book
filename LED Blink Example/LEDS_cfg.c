42137fa7cb66048c44eff84e1f9be89a9dd68dee
Tanjiro4700 <hassanomar4700@gmail.com>
2024-12-02 15:01:15 +0200

    Create LEDS_cfg.c

---
+++ LED Blink Example/LEDS_cfg.c
@@ -0,0 +1,15 @@
+#include "LEDS.h"
+
+const LED_CfgType LED_CfgParam[LED_UNITS] =
+{
+	// LED 1 Configurations
+    {
+    	GPIOB,
+		GPIO_PIN_12
+	},
+	// LED 2 Configurations
+	{
+	    GPIOB,
+		GPIO_PIN_13
+    }
+};
